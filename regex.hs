3a683ef4fbe54fdde47ff1172ab6bccc35f3db39
sid <duckworth124@github.com>
2026-01-13 22:28:57 +0000

    support for positive character groups
    also added distinction between normal and meta characters, and escaping
    support

--- regex.hs
+++ regex.hs
@@ -1,17 +1,23 @@
 import Control.Applicative
 import Control.Monad
 import Control.Monad.State
+import Data.Maybe (fromJust)
 
 type Parser a = StateT String Maybe a
-
-data RegexPattern = Literal Char
+data RegexPattern = Literal Char | PositiveGroup [Char]
+        deriving (Show)
 newtype Regex = Regex {getPatterns :: [RegexPattern]}
+        deriving (Show)
+
+runParser :: Parser a -> String -> Maybe a
+runParser = evalStateT
 
 applyPattern :: RegexPattern -> Parser String
 applyPattern (Literal c) = return <$> parseChar c
+applyPattern (PositiveGroup cs) = return <$> asum (map parseChar cs)
 
-apply :: Regex -> Parser String
-apply (Regex xs) = concat <$> mapM applyPattern xs
+applyRegex :: Regex -> Parser String
+applyRegex (Regex xs) = concat <$> mapM applyPattern xs
 
 parsePred :: (Char -> Bool) -> Parser Char
 parsePred f = do
@@ -23,6 +29,33 @@ parsePred f = do
 parseChar :: Char -> Parser Char
 parseChar = parsePred . (==)
 
--- for now we let any character through. this will change as more metacharacters are added
+parseString :: String -> Parser String
+parseString = mapM parseChar
+
+parseAny :: Parser Char
+parseAny = parsePred (const True)
+
+metacharacters :: [Char]
+metacharacters = ['[', ']', '\\']
+
+parseEscaped :: Parser Char
+parseEscaped = asum $ map (\c -> c <$ parseString ['\\', c]) metacharacters
+
+parseNotEscaped :: Parser Char
+parseNotEscaped = parsePred $ not . (`elem` metacharacters)
+
 parseLiteral :: Parser RegexPattern
-parseLiteral = Literal <$> parsePred (const True)
+parseLiteral = Literal <$> (parseNotEscaped <|> parseEscaped)
+
+parsePositiveGroup :: Parser RegexPattern
+parsePositiveGroup = parseChar '[' *> (PositiveGroup <$> some (parsePred (/= ']'))) <* parseChar ']'
+
+parsePattern :: Parser RegexPattern
+parsePattern = parsePositiveGroup <|> parseLiteral
+
+parseRegex :: Parser Regex
+parseRegex = Regex <$> many parsePattern
+
+-- helper function for quick testing in the repl
+grep :: String -> String -> Maybe String
+grep = runParser . applyRegex . fromJust . runParser parseRegex
